e7e80d5e296f87c2e2a9c1b9bd2ce793436f59dc
pratapp <pratap99@mail.ru>
2022-12-14 02:54:06 +0300

    upd

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" class="small-container">
+    <h1>Transactions:</h1>
+    <TransactionTable />
+  </div>
+</template>
+
+<script>
+import TransactionTable from './components/TransactionTable';
+
+export default {
+  name: 'App',
+
+  components: {
+    TransactionTable,
+  },
+}
+</script>
