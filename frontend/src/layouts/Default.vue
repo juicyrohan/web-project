6699507e98c2c30ce3191267861fc13e7e346f33
pratapp <pratap99@mail.ru>
2022-12-14 14:21:02 +0300

    upd

--- frontend/src/layouts/Default.vue
+++ frontend/src/layouts/Default.vue
@@ -1,20 +1,34 @@
 <template>
-  <v-app>
-    <v-navigation-drawer>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm10 md7 lg5>
-            <slot />
-          </v-flex>
-        </v-layout>
-      </v-container>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app right>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        <v-list-item link>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
-    <v-app-bar>
+    <v-app-bar app color="primary" dark>
+      <v-spacer />
+      <v-toolbar-title>Frontend application</v-toolbar-title>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
     </v-app-bar>
     <v-content>
       <slot />
     </v-content>
-    <v-footer>
+    <v-footer color="primary" app>
       <v-spacer />
       <span class="white--text">&copy; 2022</span>
     </v-footer>
