462a8453efab9bfe45bf536d211e40aa245aa652
pratapp <pratap99@mail.ru>
2022-12-16 03:47:44 +0300

    upd

--- HelloDjango/urls.py
+++ HelloDjango/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog import urls as todo_urls
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('blog.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('blog/', include(todo_urls)),
 ]
+
+handler404 = 'blog.views.handler404'
--- blog/urls.py
+++ blog/urls.py
@@ -1,18 +1,43 @@
+"""HelloDjango URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
 from django.urls import path, include
 from rest_framework import routers
 
-from core.views import CommentViewSet
+from core.views import CommentViewSet, BlogPostViewSet
 from . import views
 
-#router = routers.DefaultRouter()
-#router.register(r'comments', CommentViewSet)
+router = routers.DefaultRouter()
+router.register(r'comments', CommentViewSet)
+router.register(r'posts', BlogPostViewSet)
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-#    path('', include(router.urls)),
-    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
-    path('posts/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('posts/<int:pk>/publish/', views.post_publish, name='post_publish'),
-    path('posts/<int:pk>/comment/', views.add_comment, name='add_comment'),
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
+    path('posts/<int:id>/edit/', views.post_edit, name='post_edit'),
+    path('posts/<int:id>/publish/', views.post_publish, name='post_publish'),
+    path('posts/<int:id>/comment/', views.add_comment, name='add_comment'),
     path('posts/add/', views.post_edit, name='post_add'),
+    path('api/', include(router.urls)),
 ]
+# router = routers.DefaultRouter()
+# router.register(r'comments', CommentViewSet)
+# router.register(r'posts', BlogPostViewSet)
+
+# urlpatterns = [
+
+#      path('', include(router.urls))
+#  ]
